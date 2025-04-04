eb377b3482d494ba972d08511c6383d27c5fee5e
farzan8304 <farzankalantari.pg@gmail.com>
2025-04-04 21:29:22 +0330

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/quote": {
-        target: "http://api.quotable.io",
+        target: "https://api.quotable.io",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/quote/, "/random"),
       },
